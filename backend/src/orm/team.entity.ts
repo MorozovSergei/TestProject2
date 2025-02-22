af1c6eecde226f01d5729d2036491844f8448933
Сергей Морозов <Tyuiu.MorozovMS@outlook.com>
2025-02-22 16:20:24 +0500

    first commit

---
+++ backend/src/orm/team.entity.ts
@@ -0,0 +1,49 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  OneToMany,
+  JoinColumn,
+  ManyToMany,
+  ManyToOne,
+  OneToOne,
+} from 'typeorm';   
+
+import { User } from './user.entity';
+
+import { Role, UserAccountStatus } from 'src/common/types'; 
+  
+@Entity()
+export class Team {
+  
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column()
+    name: string;
+    
+    @Column()
+    description: string;
+    
+    @Column()
+    privacy: string;
+
+    @Column({ default: UserAccountStatus.pending })
+    status: UserAccountStatus;
+    
+    @OneToMany(() => User, (users) => users.id)
+    users: User[];
+
+    @Column({ default: '' })
+    firstname: string;
+    
+    @Column({ default: '' })
+    lastname: string;
+    
+    @Column({ type: 'varchar', default: [Role.user], array: true })
+    roles: Role[];
+    
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;    
+}
